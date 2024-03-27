dc750cae55b6222952e15c6c297bb0f7f2846140
Alessandro Canonico <alessandrocanonico96@gmail.com>
2024-03-27 16:49:37 +0100

    esercizio cercare elementi nel DOM fatto

---
+++ DOM/Cercare elementi nel DOM/dom.js
@@ -0,0 +1,11 @@
+const byClass = document.getElementsByClassName("paragrafo")
+
+console.log(byClass);
+
+const byId = document.getElementById(`elemento-3`)
+
+console.log(byId);
+
+const h2 = document.querySelectorAll("h2")
+
+console.log(h2);
