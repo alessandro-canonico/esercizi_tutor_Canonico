72084f73ad9825dabedb4e2cecf545049f2280de
Alessandro Canonico <alessandrocanonico96@gmail.com>
2024-04-03 17:44:25 +0200

    optimize esercizio 1 manipolazione DOM

--- DOM/Manipolazione del DOM/esercizio-1/index.js
+++ DOM/Manipolazione del DOM/esercizio-1/index.js
@@ -2,10 +2,10 @@ const addProduct = () => {
   if (document.querySelector("input").value.length == 0) {
     alert("insert a valid task");
   } else {
-    document.querySelector("ul").innerHTML += `
-        <li>
-            ${document.querySelector("input").value}
-            <input type="checkbox">  
-        </li>`;
+    const li = document.querySelector("ul").appendChild(document.createElement("li"));
+    li.textContent = `${document.querySelector("input").value}`;
+    const checkbox = document.createElement("input");
+    checkbox.setAttribute("type", "checkbox")
+    li.appendChild(checkbox)
   }
 };
