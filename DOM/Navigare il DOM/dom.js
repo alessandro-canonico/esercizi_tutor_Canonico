83c5e8bac760f44bcf99f9949425af7b3eda842b
Alessandro Canonico <alessandrocanonico96@gmail.com>
2024-04-03 17:15:23 +0200

    modofica esercizio navifare il dom

--- DOM/Navigare il DOM/dom.js
+++ DOM/Navigare il DOM/dom.js
@@ -4,10 +4,10 @@ const body = html.children[1]
 
 const ul = body.children[0]
 
-console.log(ul.parentElement.innerText);
+console.log(ul.parentElement);
 
 const liSecond = ul.children[1]
 
-console.log(liSecond.innerText);
-console.log(liSecond.nextElementSibling.innerText);
-console.log(liSecond.previousElementSibling.innerText);
+console.log(liSecond);
+console.log(liSecond.nextElementSibling);
+console.log(liSecond.previousElementSibling);
