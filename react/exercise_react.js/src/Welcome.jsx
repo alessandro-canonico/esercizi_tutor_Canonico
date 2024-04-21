0bc00d8382a232590ef04e08b2ecd87fb5750e8f
Alessandro Canonico <alessandrocanonico96@gmail.com>
2024-04-21 12:41:48 +0200

    props excercise done

---
+++ react/exercise_react.js/src/Welcome.jsx
@@ -0,0 +1,12 @@
+import { Age } from "./Age";
+
+export function Welcome ({ name, age }) {
+    return ( <div>
+        <p>Welcome, { name }</p>
+        {<Age age={age}/>}
+        {age >= 18 && <Age age={age}/>}
+        {age && <Age age={age}/>}
+        {age >= 18 && age <= 65 && <Age age={age}/>}
+        {age >= 18 && age <= 65 && name == `John` && <Age age={age}/>}
+    </div>)
+}
