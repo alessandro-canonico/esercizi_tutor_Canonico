cdcfd2becb2ecbfe981fec4ab6d84f8d02ff1cfa
Alessandro Canonico <alessandrocanonico96@gmail.com>
2024-04-30 18:21:18 +0200

    exercise component composition done

--- react/exercise_react.js/src/App.jsx
+++ react/exercise_react.js/src/App.jsx
@@ -1,25 +1,27 @@
 import { AlertClock } from "./AlertClock";
 import { Clock } from "./Clock";
+import { Container } from "./Container";
 import { Counter } from "./Counter";
 import { MouseClicker } from "./MouseClicker";
 import { Welcome } from "./Welcome";
 
 export function App() {
   return (
-    <div>
-      <h1>Esercizi: </h1>
-      <hr></hr>
-
+    <Container title={<h1>Esercizi - Alessandro Canonico</h1>}>
       <Welcome name="John" age={27} />
-      
-      <AlertClock/>
-
-      <Counter initialValue={1} incrementValue={(c) => c+1} decrementValue={(c) => c-1}/>
-
+      <hr />
+      <AlertClock />
+      <hr />
+      <Counter
+        initialValue={1}
+        incrementValue={(c) => c + 1}
+        decrementValue={(c) => c - 1}
+      />
+      <hr />
       <h2>Clock:</h2>
-      <Clock/>
-
-      <MouseClicker/>
-    </div>
+      <Clock />
+      <hr />
+      <MouseClicker />
+    </Container>
   );
 }
---
+++ react/exercise_react.js/src/Container.jsx
@@ -0,0 +1,17 @@
+import { useState } from "react";
+
+export function Container({ title, children }) {
+    const [collapsed, setCollapased] = useState(false)
+
+    function handleCollapse () {
+        setCollapased((data) => !data)
+    }
+
+
+  return (
+    <div className="container">
+      <div className="container-title">{title} <button onClick={handleCollapse}>Show the exercises</button> </div>
+      <div className={!collapsed ? "container-content" : "show-content"}>{children}</div>
+    </div>
+  );
+}
