35644b9affc28ebf919256dfed3ff263866c70f5
Alessandro Canonico <alessandrocanonico96@gmail.com>
2024-05-03 12:49:27 +0200

    riorganizzazione cartelle ed esercizi

--- react/exercise_react.js/src/App.jsx
+++ react/exercise_react.js/src/App.jsx
@@ -1,34 +1,25 @@
-import { AlertClock } from "./AlertClock";
-import { Clock } from "./Clock";
+import { AlertClock } from "./Events/AlertClock";
+import { Clock } from "./useState - useEffect/Clock";
 import { Container } from "./Container";
-import { Counter } from "./Counter";
-import { MouseClicker } from "./MouseClicker";
-import { Welcome } from "./Welcome";
-import { LanguageContext } from "./LanguageContext";
-import { useState } from "react";
+import { Counter } from "./useState - useEffect/Counter";
+import { MouseClicker } from "./Handling Events/MouseClicker";
+import { Welcome } from "./Conditional Rendering/Welcome";
 
 export function App() {
-  const [language, setLanguage] = useState(`en`);
-
-  function handleLanguage() {
-    setLanguage(language);
-  }
-
   return (
     <div>
-      <div>
+      {/* <div>
       <label htmlFor="languages"></label>
       <select name="languages" id="languages">
         <option value="en"><button onClick={() => handleLanguage(`en`)}>English</button></option>
         <option value="it" onClick={() => handleLanguage(`it`)}>Italian</option>
       </select>
-      </div>
+      </div> */}
 
       <Container title={<h1>Esercizi - Alessandro Canonico</h1>}>
-        <LanguageContext.Provider value={language}>
           <Welcome name="John" age={27} />
           <hr />
-          <AlertClock />
+          <AlertClock/>
           <hr />
           <Counter
             initialValue={1}
@@ -40,7 +31,6 @@ export function App() {
           <Clock />
           <hr />
           <MouseClicker />
-        </LanguageContext.Provider>
       </Container>
     </div>
   );
---
+++ react/exercise_react.js/src/useState - useEffect/Clock.jsx
@@ -0,0 +1,18 @@
+import { useEffect, useState } from "react";
+
+
+export function Clock () {    
+    const [date, setDate] = useState(new Date())
+
+    useEffect(() => {
+        setInterval(() => {
+        setDate(new Date())}, 1000)
+    }, [])
+
+    return (
+        <div className="clock">
+            <h4>Current Time</h4>
+            <p>{date.toLocaleTimeString()}</p> 
+        </div>
+    ) 
+}
