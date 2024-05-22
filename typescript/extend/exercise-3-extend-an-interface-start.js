bcd6d55a3348891e860a67a80b7ed46efdebb284
Alessandro Canonico <alessandrocanonico96@gmail.com>
2024-05-22 18:32:57 +0200

    object types and extend ex done

---
+++ typescript/extend/exercise-3-extend-an-interface-start.js
@@ -0,0 +1,23 @@
+"use strict";
+/**
+ * START: Follow the instructions below.
+ */
+Object.defineProperty(exports, "__esModule", { value: true });
+var countryA = {
+    name: "India",
+    code: "IN",
+    population: 1352642280,
+};
+var countryB = {
+    name: "Italy",
+    code: "IT"
+};
+var countryC = {
+    name: "Spain",
+    code: "ES",
+    population: 47450795,
+};
+// ----
+console.log(countryA);
+console.log(countryB);
+console.log(countryC);
